1c2d38d509bc9de3c631912d4ca0bec35728c0fd
beingkashifali <beingkashifali@gmail.com>
2026-09-16 23:46:12 +0500

    updated features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Home from "./pages/Homes";
+import Footer from "./components/layout/Footer";
+
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,8 +15,18 @@ function App() {
       <div className="flex-1">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products/:slug" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/" element={}/>
+          <Route path="/" element={}/>
+          <Route path="/" element={}/>
+          <Route path="/" element={}/>
+          <Route path="/" element={}/> */}
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
